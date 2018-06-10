4218f8b0ec1d3c1e4e4f4b68be048e4b1df05f00
twoUTF <semmarkus@gmail.com>
2018-06-10 19:42:05 +0200

    Eindproject 2nd commit

--- JAVASCRIPT/Main.js
+++ JAVASCRIPT/Main.js
@@ -15,112 +15,29 @@ function MenuUp() {
 }
 
 
+var slideIndex = 1;
+showSlides(slideIndex);
 
-
-
-
-
-//1. set ul width
-//2. image when click prev/next button
-var ul;
-var liItems;
-var imageNumber;
-var imageWidth;
-var prev, next;
-var currentPostion = 0;
-var currentImage = 0;
-
-
-function init(){
-    ul = document.getElementById('image_slider');
-    liItems = ul.children;
-    imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].clientWidth;
-    ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
-    prev = document.getElementById("prev");
-    next = document.getElementById("next");
-    generatePager(imageNumber);
-    //.onclike = slide(-1) will be fired when onload;
-    /*
-    prev.onclick = function(){slide(-1);};
-    next.onclick = function(){slide(1);};*/
-    prev.onclick = function(){ onClickPrev();};
-    next.onclick = function(){ onClickNext();};
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function animate(opts){
-    var start = new Date;
-    var id = setInterval(function(){
-        var timePassed = new Date - start;
-        var progress = timePassed / opts.duration;
-        if (progress > 1){
-            progress = 1;
-        }
-        var delta = opts.delta(progress);
-        opts.step(delta);
-        if (progress == 1){
-            clearInterval(id);
-            opts.callback();
-        }
-    }, opts.delay || 17);
-    //return id;
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
-function slideTo(imageToGo){
-    var direction;
-    var numOfImageToGo = Math.abs(imageToGo - currentImage);
-    // slide toward left
-
-    direction = currentImage > imageToGo ? 1 : -1;
-    currentPostion = -1 * currentImage * imageWidth;
-    var opts = {
-        duration:1000,
-        delta:function(p){return p;},
-        step:function(delta){
-            ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
-        },
-        callback:function(){currentImage = imageToGo;}
-    };
-    animate(opts);
-}
-
-function onClickPrev(){
-    if (currentImage == 0){
-        slideTo(imageNumber - 1);
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    else{
-        slideTo(currentImage - 1);
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
-
-function onClickNext(){
-    if (currentImage == imageNumber - 1){
-        slideTo(0);
-    }
-    else{
-        slideTo(currentImage + 1);
-    }
-}
-
-function generatePager(imageNumber){
-    var pageNumber;
-    var pagerDiv = document.getElementById('pager');
-    for (i = 0; i < imageNumber; i++){
-        var li = document.createElement('li');
-        pageNumber = document.createTextNode(parseInt(i + 1));
-        li.appendChild(pageNumber);
-        pagerDiv.appendChild(li);
-        // add event inside a loop, closure issue.
-        li.onclick = function(i){
-            return function(){
-                slideTo(i);
-            }
-        }(i);
-    }
-    var computedStyle = document.defaultView.getComputedStyle(li, null);
-    //border width 1px; offsetWidth = 22
-    var liWidth = parseInt(li.offsetWidth);
-    var liMargin = parseInt(computedStyle.margin.replace('px',""));
-    pagerDiv.style.width = parseInt((liWidth + liMargin * 2) * imageNumber) + 'px';
-}
-window.onload = init;
